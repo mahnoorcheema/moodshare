5503ed1826b28d5ee2e04cfae26f5c3f36664c82
mahnoorcheema <mchee050@uottawa.ca>
2019-06-26 17:18:07 -0400

    Redirects users to login page when not logged in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import firebase from 'firebase/app';
 import "firebase/firestore";
@@ -12,25 +12,54 @@ import MoodCalendar from "./components/MoodCalendar"
 
 firebase.initializeApp(firebaseConfig);
 
+const useCurrentUser = () => {
+  const [user, setUser] = useState(null);
+  useEffect(() => firebase.auth().onAuthStateChanged(setUser), []);
+  return user;
+};
+
+const AuthenticatedRoute = ({ component: Component, user,...rest }) => (
+  <Route {...rest} render={(props) => (
+    user
+      ? <Component {...props} />
+      : <main>
+        Looks like you've signed out, please <Link to="/login">login again</Link>.
+      </main>
+  )} />
+);
+
+// const SignOut = async () => {
+//   firebase.auth().signOut().then(function () {
+//     console.log("sign out successful")
+//   }).catch(function (error) {
+//     console.log(error.code, error.body);
+//   })
+// };
+
+//Todo: Implement logout function
 const App = () => {
+  const user = useCurrentUser();
+
   return <>
-    <h1>Mood Share</h1>
     <Router>
-      <ul>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/moods">Current Mood</Link>
-        </li>
-        <li>
-          <Link to="/calendar">Mood Calendar</Link>
-        </li>
-      </ul>
+      <nav>
+        <ul className="nav-bar">
+          <h1>M☺☻d Share</h1>
+          <li>
+            {!user ?
+            <Link to="/login" className="nav-bar-element">Login</Link>
+            : <Link to="/login" className="nav-bar-element">Logout</Link>}
+          </li>
+          <li>
+            <Link to="/calendar" className="nav-bar-element">Mood Calendar</Link>
+          </li>
+        </ul>
+      </nav>
       <Route path="/login" component={Login}/>
-      <Route path="/moods" component={CurrentMoods}/>
-      <Route path="/calendar" component={MoodCalendar}/>
       <Route path="/create-account" component={CreateAccount}/>
+
+      <AuthenticatedRoute user={user} path="/moods" component={CurrentMoods}/>
+      <AuthenticatedRoute user={user} path="/calendar" component={MoodCalendar}/>
     </Router>
   </>
 };
